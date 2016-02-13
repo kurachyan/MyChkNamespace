af6c334303ac38bbd61a4d20e4a6d7cf84e8a498
kurachyan <e_tamakou@outlook.jp>
2016-02-13 22:36:49 +0900

    初期設定１

--- ChkNamespace/CS_ChkNamespace.cs
+++ ChkNamespace/CS_ChkNamespace.cs
@@ -11,6 +11,7 @@ namespace ChkNamespace
     {
         #region 共有領域
         CS_LRskip lrskip;           // 両側余白情報を削除
+        CS_Rsvwrd rsvwrd;           // 予約語を確認する
 
         private String _wbuf;
         private Boolean _empty;
@@ -49,13 +50,26 @@ namespace ChkNamespace
                 }
             }
         }
+        private String _result;     // [Namespace]ＬＢＬ情報
+        public String Result
+        {
+            get
+            {
+                return (_result);
+            }
+            set
+            {
+                _result = value;
+            }
+        }
 
         // 予約語：「ＮａｍｅＳｐａｃｅ」
         const int RSV_NONE = 0;     // 未定義
         const int RSV_OTHER = 6;    // 予約語６：その他
-        const string RSV_KEYWORD = "namespace";
-        private int _rsvcode;
+//        const string RSV_KEYWORD = "namespace";
+//        private int _rsvcode;
 
+/*
         public Boolean Rsv
         {   // 予約語有無確認　[false]:予約語なし [true]:予約語あり
             get
@@ -84,31 +98,70 @@ namespace ChkNamespace
                 return ((int)_rsvcode);
             }
         }
-        #endregion
+*/
 
-        #region コンストラクタ
+        private static Boolean _Is_namespace;
+        public Boolean Is_namespace
+        {
+            get
+            {
+                return (_Is_namespace);
+            }
+            set
+            {
+                _Is_namespace = value;
+            }
+        }
+#endregion
+
+#region コンストラクタ
         public CS_ChkNamespace()
         {   // コンストラクタ
             _wbuf = null;       // 設定情報無し
             _empty = true;
 
-            _rsvcode = RSV_NONE;    // 予約語：未定義
-        }
-        #endregion
+//            _rsvcode = RSV_NONE;    // 予約語：未定義
+            _Is_namespace = false;  // [namespace]フラグ：false
 
-        #region モジュール
+            rsvwrd = new CS_Rsvwrd();           // 予約語を確認する
+        }
+#endregion
+
+#region モジュール
         public void Clear()
         {   // 作業領域の初期化
             _wbuf = null;       // 設定情報無し
             _empty = true;
 
-            _rsvcode = RSV_NONE;    // 予約語：未定義
+//            _rsvcode = RSV_NONE;    // 予約語：未定義
+            _Is_namespace = false;  // [namespace]フラグ：false
         }
 
         public void Exec()
         {   // "namespace"評価
+            if (!_empty)
+            {   // バッファーに実装有り
+                rsvwrd.Exec(_wbuf);     // 評価情報の予約語確認を行う
+
+                if (_Is_namespace)
+                {   // [namespace]フラグは、true？
+                    if (!rsvwrd.Is_namespace)
+                    {   // 評価情報は、非予約語？
+                        // ＬＢＬ情報テーブルに、namespace名を登録する
+                        _Is_namespace = false;       // [namespace]フラグ：false
+                    }
+                }
+                else
+                {   // [namespace]フラグは、false
+                    if (rsvwrd.Is_namespace)
+                    {   // 評価情報は、"namespace"？
+                        _Is_namespace = true;       // [namespace]フラグ：true
+                        rsvwrd.Is_namespace = false;
+                    }
+                }
+            }
         }
-        #endregion
+#endregion
 
     }
 }
